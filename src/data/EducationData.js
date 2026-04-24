f0851d2b2096efe1f2e51915b323293823d97fcc
cleandev-repo <229561671+cleandev-repo@users.noreply.github.com>
2026-04-24 08:36:41 -0700

    updated education

--- src/data/EducationData.js
+++ src/data/EducationData.js
@@ -1,6 +1,6 @@
 const EducationData = [
     {
-        degree: "Engineering Degree, Information Technology",
+        degree: "Engineering Degree, Computer Science",
         university: "Universidad Politécnica del Estado de Morelos (Upemor) de México",
         date: "2013 - 2017",
         location: "Morelos, Mexico",
